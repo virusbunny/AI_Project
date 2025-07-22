d23ee1663659e09a63fe5c2276b8a16010479de6
Shushant kumar <142226359+virusbunny@users.noreply.github.com>
2025-07-22 12:22:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
